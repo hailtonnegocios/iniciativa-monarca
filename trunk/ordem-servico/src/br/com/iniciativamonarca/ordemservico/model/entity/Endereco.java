bbcc78ddc25a423c89fbb0636f5ce16be6259fba
hailtonnegocios@gmail.com <hailtonnegocios@gmail.com@76a35db3-af0d-6f06-19cc-76fcdb474f56>
2014-06-26 21:00:24 +0000

    Commit: organização dos construtores na classe Endereco

--- trunk/ordem-servico/src/br/com/iniciativamonarca/ordemservico/model/entity/Endereco.java
+++ trunk/ordem-servico/src/br/com/iniciativamonarca/ordemservico/model/entity/Endereco.java
@@ -6,7 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Embeddable
 @XmlRootElement(name = "webservicecep")
 public class Endereco {
-	
+
 	private String resultado;
 	private String resultado_txt;
 	private String uf;
@@ -17,8 +17,23 @@ public class Endereco {
 	private String numero;
 	private String cep;
 	private String complemento;
-	
-	
+
+	public Endereco() {
+
+	}
+
+	public Endereco(String numero, String complemento, String uf,
+			String cidade, String bairro, String tipo_logradouro,
+			String logradouro) {
+		this.numero = numero;
+		this.complemento = complemento;
+		this.uf = uf;
+		this.cidade = cidade;
+		this.bairro = bairro;
+		this.tipo_logradouro = tipo_logradouro;
+		this.logradouro = logradouro;
+	}
+
 	public String getCep() {
 		return cep;
 	}
@@ -43,65 +58,60 @@ public class Endereco {
 		this.resultado_txt = resultado_txt;
 	}
 
-	public Endereco(){
-		
-	}
-	
-	public Endereco(String numero,String complemento,String uf,String cidade,String bairro,String tipo_logradouro,String logradouro){
-
-		this.numero = numero;
-		this.complemento = complemento;
-		this.uf = uf;
-		this.cidade = cidade;
-		this.bairro = bairro;
-		this.tipo_logradouro = tipo_logradouro;
-		this.logradouro = logradouro;
-		
-	}
-	
-	
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public String getComplemento() {
 		return complemento;
 	}
+
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
+
 	public String getUf() {
 		return uf;
 	}
+
 	public void setUf(String uf) {
 		this.uf = uf;
 	}
+
 	public String getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
+
 	public String getTipo_logradouro() {
 		return tipo_logradouro;
 	}
+
 	public void setTipo_logradouro(String tipo_logradouro) {
 		this.tipo_logradouro = tipo_logradouro;
 	}
+
 	public String getLogradouro() {
 		return logradouro;
 	}
+
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-	
 
 }
