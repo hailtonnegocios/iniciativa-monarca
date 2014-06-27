8db6d38ff22c60760b623ca455586bf067ca5c18
hailtonnegocios@gmail.com <hailtonnegocios@gmail.com@76a35db3-af0d-6f06-19cc-76fcdb474f56>
2014-06-27 02:19:53 +0000

    Commit: Inclusão do List<String>Telefones e List<String>Celulares na classe Funcionario

--- ordem-servico/src/br/com/iniciativamonarca/ordemservico/model/entity/Funcionario.java
+++ ordem-servico/src/br/com/iniciativamonarca/ordemservico/model/entity/Funcionario.java
@@ -1,5 +1,8 @@
 package br.com.iniciativamonarca.ordemservico.model.entity;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
@@ -12,42 +15,70 @@ public class Funcionario extends Usuario {
 	String cpf;
 	String rg;
 	String setor;
-	
+
+	@ElementCollection
+	List<String> telefones;
+
+	@ElementCollection
+	List<String> celulares;
+
 	@Embedded
 	Endereco endereco;
-	
+
+	public List<String> getCelulares() {
+		return celulares;
+	}
+
+	public void setCelulares(List<String> celulares) {
+		this.celulares = celulares;
+	}
+
+	public List<String> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(List<String> telefones) {
+		this.telefones = telefones;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getRg() {
 		return rg;
 	}
+
 	public void setRg(String rg) {
 		this.rg = rg;
 	}
+
 	public String getSetor() {
 		return setor;
 	}
+
 	public void setSetor(String setor) {
 		this.setor = setor;
 	}
+
 	public Endereco getEndereco() {
 		return endereco;
 	}
+
 	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
 
-	
-	
-	
 }
