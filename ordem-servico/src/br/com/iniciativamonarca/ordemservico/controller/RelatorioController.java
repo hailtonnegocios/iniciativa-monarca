076d14c67a948721dd4a9406f01adf4c83755128
hailtonnegocios@gmail.com <hailtonnegocios@gmail.com@76a35db3-af0d-6f06-19cc-76fcdb474f56>
2014-08-13 02:42:50 +0000

    Commit: Criação de rotinas para gerar Relatorio e exportar para PDF,XLS,CSV,XML e HTML

--- ordem-servico/src/br/com/iniciativamonarca/ordemservico/controller/RelatorioController.java
+++ ordem-servico/src/br/com/iniciativamonarca/ordemservico/controller/RelatorioController.java
@@ -9,27 +9,52 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import br.com.iniciativamonarca.ordemservico.model.dao.ConnectionFactory;
+import br.com.iniciativamonarca.ordemservico.model.dao.impl.FuncionarioDAO;
 import br.com.iniciativamonarca.ordemservico.relatorios.GeradorRelatorio;
 
 
 @Controller
 public class RelatorioController {
 
+	@Autowired
+	FuncionarioDAO funcionario ;
+	
+	
 	@RequestMapping("relatorioTeste")
 	public void geraRelatorio(HttpServletRequest request, HttpServletResponse response) throws ClassNotFoundException, FileNotFoundException, JRException, IOException{
-		
 		Map<String, Object> parameters = new HashMap<String, Object>();
+
+
+		GeradorRelatorio geradorDeRelatorio = new GeradorRelatorio("usuarios.jrxml",request,response,parameters);
+		// Carrega lista no Relatorio para dar o Print
+		JasperPrint print = geradorDeRelatorio.atribuiListaCarregadaNoRelatorio(funcionario.listar());
+		// Visualiza relatorio no navegador com formato PDF
+		geradorDeRelatorio.visualizarRelatorio(print,response);
 		
-		String nome = request.getServletContext().getRealPath("/WEB-INF/relatorios/usuarios.jasper");
-		
-		GeradorRelatorio geradorRelatorio = new GeradorRelatorio(nome, parameters , new ConnectionFactory().getConnection());
-		
-		geradorRelatorio.geraPDF(response.getOutputStream());
+		// Exportar Relatorios
+		String pdf = "C:/Users/Hailton/Desktop/Tela_Funcionario/Funcionarios.pdf";
+		String xml = "C:/Users/Hailton/Desktop/Tela_Funcionario/Funcionarios.xml";
+		String excel = "C:/Users/Hailton/Desktop/Tela_Funcionario/Funcionarios.xls";
+		String csv = "C:/Users/Hailton/Desktop/Tela_Funcionario/Funcionarios.csv";
+		String html = "C:/Users/Hailton/Desktop/Tela_Funcionario/Funcionarios.html";
+
+		geradorDeRelatorio.exportarRelatorioNoFormatoHTML(print,html);
+		geradorDeRelatorio.exportarRelatorioNoFormatoPDF(print,pdf);
+		geradorDeRelatorio.exportarRelatorioNoFormatoXLS(print,excel);
+		geradorDeRelatorio.exportarRelatorioNoFormatoCSV(print,csv);
+		geradorDeRelatorio.exportarRelatorioNoFormatoXML(print,xml,false);
 		
 	}
 	
--- ordem-servico/src/br/com/iniciativamonarca/ordemservico/relatorios/GeradorRelatorio.java
+++ ordem-servico/src/br/com/iniciativamonarca/ordemservico/relatorios/GeradorRelatorio.java
@@ -1,43 +1,138 @@
 package br.com.iniciativamonarca.ordemservico.relatorios;
 
-import java.io.FileNotFoundException;
-import java.io.OutputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporter;
 import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
 
 public class GeradorRelatorio {
-	
-	String nome;
+
+	HttpServletRequest request;
+	HttpServletResponse response;
 	Map<String, Object> parameters;
-	Connection connection;
-	
-	
-	
-	
-	public GeradorRelatorio(String nome, Map<String, Object> parameters,
-			Connection connection) {
+	String nome_arquivo;
+
+	public GeradorRelatorio(String nome_arquivo, HttpServletRequest request,
+			HttpServletResponse response, Map<String, Object> parameters) {
 		super();
-		this.nome = nome;
 		this.parameters = parameters;
-		this.connection = connection;
+		this.request = request;
+		this.response = response;
+		this.nome_arquivo = nome_arquivo;
 	}
 
-
-
-
-	public void geraPDF(OutputStream output) throws JRException, FileNotFoundException {
-		JasperPrint print = JasperFillManager.fillReport(nome, parameters , connection);
-		JRExporter exporter = new JRPdfExporter();
-		exporter.setParameter(JRExporterParameter.JASPER_PRINT, print);
-		exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, output);
-		exporter.exportReport();
+	public JasperReport retornaArquivoCompilado(HttpServletRequest request) {
+		try {
+			// Compila o arquivo JRXML
+			JasperReport report = JasperCompileManager.compileReport(request
+					.getServletContext().getRealPath(
+							"/WEB-INF/relatorios/" + nome_arquivo));
+			return report;
+		} catch (JRException e) {
+			System.out.println("Erro ao compilar arquivo");
+			e.printStackTrace();
+			return null;
+		}
 	}
+
+	// Passagem da lista para o relatório
+	public JasperPrint atribuiListaCarregadaNoRelatorio(List<?> lista) {
+		try {
+			JasperPrint print = JasperFillManager.fillReport(
+					retornaArquivoCompilado(request), parameters,
+					new JRBeanCollectionDataSource(lista));
+			return print;
+		} catch (JRException e) {
+			System.out.println("Erro ao colocar a lista no relatório");
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void visualizarRelatorio(JasperPrint print,
+			HttpServletResponse response) {
+		try {
+			JasperExportManager.exportReportToPdfStream(print,response.getOutputStream());
+			System.out.println("Relatório gerado.");
+		} catch (JRException | IOException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao gerar relatorio.");
+		}
+	}
+
+	public void exportarRelatorioNoFormatoHTML(JasperPrint print, String destino) {
+		try {
+			JasperExportManager.exportReportToHtmlFile(print, destino);
+			System.out.println("Relatório HTML exportado.");
+		} catch (JRException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao exportar relatorio em HTML");
+		}
+	}
+
+	public void exportarRelatorioNoFormatoPDF(JasperPrint print, String destino) {
+		try {
+			JasperExportManager.exportReportToPdfFile(print, destino);
+			System.out.println("Relatório PDF exportado.");
+		} catch (JRException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao exportar relatorio em PDF");
+		}
+	}
+
+	public void exportarRelatorioNoFormatoXML(JasperPrint print,
+			String destino, Boolean possuiFoto) {
+		try {
+			JasperExportManager.exportReportToXmlFile(print, destino,possuiFoto);
+			System.out.println("Relatório XML exportado.");
+		} catch (JRException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao exportar relatorio em XML");
+		}
+	}
+
+	public void exportarRelatorioNoFormatoXLS(JasperPrint print, String destino) {
+		try {
+			JRXlsExporter exporterXLS = new JRXlsExporter();
+			exporterXLS.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			exporterXLS.setParameter(JRExporterParameter.OUTPUT_FILE_NAME,destino);
+			/*exporterXLS.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET, Boolean.TRUE);
+			exporterXLS.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
+			exporterXLS.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+			exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);*/
+			exporterXLS.exportReport();
+			System.out.println("Relatório XLS exportado.");
+		} catch (JRException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao exportar relatorio em XLS");
+		}
+	}
+
+	public void exportarRelatorioNoFormatoCSV(JasperPrint print, String destino) {
+		try {
+			JRCsvExporter exporterCsv = new JRCsvExporter();
+			exporterCsv.setParameter(JRExporterParameter.JASPER_PRINT, print);
+			exporterCsv.setParameter(JRExporterParameter.OUTPUT_FILE_NAME,destino);
+			exporterCsv.exportReport();
+			System.out.println("Relatório CSV exportado.");
+		} catch (JRException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao exportar relatorio em CSV");
+		}
+	}
+
 }
