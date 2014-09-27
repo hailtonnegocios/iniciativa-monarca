a45b71e369a295634dd3d60129ead5911f9cfc56
luceliocampoi@gmail.com <luceliocampoi@gmail.com@76a35db3-af0d-6f06-19cc-76fcdb474f56>
2014-09-27 19:05:09 +0000

    Adicionado o modulo de clientes

---
+++ trunk/ordem-servico/src/br/com/iniciativamonarca/ordemservico/controller/ClienteController.java
@@ -0,0 +1,29 @@
+package br.com.iniciativamonarca.ordemservico.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.iniciativamonarca.ordemservico.model.dao.impl.ClienteDAO;
+import br.com.iniciativamonarca.ordemservico.model.entity.Cliente;
+
+
+@Transactional
+@Controller
+public class ClienteController {
+		
+	@Autowired
+	ClienteDAO clienteDao;
+	
+	@RequestMapping ("adicionaCliente")
+	public void adicionaCliente (@Valid Cliente cliente){
+		System.out.println(cliente);
+		clienteDao.adicionar(cliente);
+	}
+	
+	
+	
+}
